baa280f1773bd191d436666df2ba64aebd4823e0
festuskiambi <kiamfestoh@gmail.com>
2024-03-13 10:04:44 +0300

    test erase overlapping intervals.
    Signed-off-by: festuskiambi <kiamfestoh@gmail.com>

--- src/Intervals/ElaseOverlappingIntervals.kt
+++ src/Intervals/ElaseOverlappingIntervals.kt
@@ -1,6 +1,26 @@
 package Intervals
 
+
+/**
+ * Given an array of intervals intervals where intervals[i] = [starti, endi],
+ * return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+ */
+fun main(){
+
+    // expect 1
+
+    val test = arrayOf(
+        intArrayOf(1, 2),
+        intArrayOf(2, 3),
+        intArrayOf(3, 4),
+        intArrayOf(1, 3),
+    )
+
+    println(eraseOverlapIntervals(test))
+}
 fun eraseOverlapIntervals(intervals: Array<IntArray>): Int {
+
+    intervals.sortBy { it.first() }
     var count = 0
 
     var prevEnd = intervals[0][1]
