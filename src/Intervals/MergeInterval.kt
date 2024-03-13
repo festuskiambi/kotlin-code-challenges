40860cc5dd5e601f33c6209087158a95e6444487
festuskiambi <kiamfestoh@gmail.com>
2024-03-13 09:50:57 +0300

    test merge interval.
    Signed-off-by: festuskiambi <kiamfestoh@gmail.com>

--- src/Intervals/MergeInterval.kt
+++ src/Intervals/MergeInterval.kt
@@ -2,14 +2,33 @@ package Intervals
 
 import java.util.*
 
+/**
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+ * and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ */
+
+fun main() {
+    // expect [[1,6],[8,10],[15,18]]
+
+    val test = arrayOf(
+        intArrayOf(1, 3),
+        intArrayOf(2, 6),
+        intArrayOf(8, 10),
+        intArrayOf(15, 18),
+    )
+
+
+    println(merge(test).map { it.contentToString() })
+}
+
 fun merge(intervals: Array<IntArray>): Array<IntArray> {
 
     intervals.sortBy { it.first() }
 
     val res = LinkedList<IntArray>()
 
-    for (interval in intervals){
-        if (res.size ==0 || res.last[1] < interval[0]){
+    for (interval in intervals) {
+        if (res.size == 0 || res.last[1] < interval[0]) {
             res.add(interval)
         } else {
             res.last[1] = maxOf(res.last[1], interval[1])
